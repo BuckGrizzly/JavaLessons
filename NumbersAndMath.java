1002a9fa7b550a8aa0d916aeee83f9edb2a8114a
mjennison <mjenniso@c42c03257f9d.ant.amazon.com>
2014-07-27 17:12:43 -0700

    Variables assignments

--- NumbersAndMath.java
+++ NumbersAndMath.java
@@ -2,27 +2,46 @@ public class NumbersAndMath
 {
 	public static void main( String[] args )
 	{
+		//Prints text in quotes
 		System.out.println( "I will now count my chickens:");
+		
+		//Prints text and calculates sum
 		System.out.println( "Hens " + ( 25 + 30 / 6 ));
+		
+		//Prints text and calculates sum
 		System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ));
 		
+		//Prints text in quotes
 		System.out.println( "I will now count my eggs:");
 		
+		//Calculates sum
 		System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6);
 		
+		//Prints text and calculates sum
 		System.out.println( "Is it true that 3 + 2 < 5 - 7?");
 		
+		//Calculates sum
 		System.out.println( 3 + 2 < 5 - 7);
 		
+		//Prints text and calculates sum
 		System.out.println( "What is 3 + 2? " + (3 + 2));
+		
+		//Prints text and calculates sum
 		System.out.println( "What is 5 - 7? " + (5 - 7));
 		
+		//Prints text in quotes
 		System.out.println( "Oh, that's why its false." );
 		
+		//Prints text in quotes
 		System.out.println( "How about some more." );
 		
+		//Prints text and calculates sum
 		System.out.println( "Is it greater? " + ( 5 > -2 ) );
+		
+		//Prints text and calculates sum
 		System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+		
+		//Prints text and calculates sum
 		System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
 	}
 }
